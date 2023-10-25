b5c9b75c808882c00ba5f13946a2f4a89dcc75ff
project-java-reza <rizqirezaardiansyahjava@gmail.com>
2023-10-25 10:41:12 +0700

    Initial Project

---
+++ src/main/java/com/enigma/tokonyareza/TokonyarezaApplication.java
@@ -0,0 +1,13 @@
+package com.enigma.tokonyareza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokonyarezaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TokonyarezaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/enigma/tokonyareza/TokonyarezaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enigma.tokonyareza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TokonyarezaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
