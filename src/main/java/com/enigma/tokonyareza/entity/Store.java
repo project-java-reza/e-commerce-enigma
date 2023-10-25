901691763809f4436055de66076c2e72ad45ef51
project-java-reza <rizqirezaardiansyahjava@gmail.com>
2023-10-25 15:04:36 +0700

    [FEATURE[ add new file entity

---
+++ src/main/java/com/enigma/tokonyareza/entity/Store.java
@@ -0,0 +1,28 @@
+package com.enigma.tokonyareza.entity;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "m_store")
+public class Store {
+
+    @Id
+    @GenericGenerator(strategy = "uuid2", name = "system-uuid")
+    @GeneratedValue(generator = "system-uuid")
+    private String id;
+
+    @Column(name = "no_siup", unique = true, nullable = false, length = 100) // no siup tidak akan mungkin bisa sama karena unique
+    private String noSiup;
+
+    @Column(length = 100, nullable = false)
+    private String name;
+
+    @Column(length = 100, nullable = false)
+    private String address;
+
+    @Column(name ="mobile_phone", unique = true, nullable = false)
+    private String mobilePhone;
+
+ }
