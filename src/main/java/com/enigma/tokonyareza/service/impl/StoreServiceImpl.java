5c562cf3ad01da11d02833b48577e86d1907fb83
project-java-reza <rizqirezaardiansyahjava@gmail.com>
2023-10-27 12:28:45 +0700

    [IMPROVE] new code getAllResponse();

--- src/main/java/com/enigma/tokonyareza/service/impl/StoreServiceImpl.java
+++ src/main/java/com/enigma/tokonyareza/service/impl/StoreServiceImpl.java
@@ -1,9 +1,12 @@
 package com.enigma.tokonyareza.service.impl;
 import com.enigma.tokonyareza.entity.Store;
+import com.enigma.tokonyareza.model.response.StoreResponse;
 import com.enigma.tokonyareza.repository.StoreRepository;
 import com.enigma.tokonyareza.service.StoreService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -41,5 +44,19 @@ public class StoreServiceImpl implements StoreService {
         storeRepository.deleteById(id);
     }
 
+    @Override
+    public List<StoreResponse> getAllResponse() {
 
+        List<Store> stores = storeRepository.findAll();
+        List<StoreResponse> responses = new ArrayList<>();
+        for (Store store : stores) {
+            StoreResponse response = StoreResponse.builder()
+                    .id(store.getId())
+                    .storeName(store.getName())
+                    .address(store.getAddress())
+                    .build();
+            responses.add(response);
+        }
+        return responses;
+    }
 }
