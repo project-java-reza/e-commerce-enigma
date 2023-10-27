ab2ff3eb4259ec3e0bd13db716d1ab56b4d6bae8
project-java-reza <rizqirezaardiansyahjava@gmail.com>
2023-10-27 11:55:29 +0700

    repair

---
+++ src/main/java/com/enigma/tokonyareza/repository/CustomerRepository.java
@@ -0,0 +1,10 @@
+package com.enigma.tokonyareza.repository;
+
+import com.enigma.tokonyareza.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, String> {
+    boolean existsByEmail(String email);
+}
