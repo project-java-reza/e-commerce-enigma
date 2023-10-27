2a57f0ecb2d1af3a5ff8ce0a9d693c5d77b15761
project-java-reza <rizqirezaardiansyahjava@gmail.com>
2023-10-27 12:19:52 +0700

    [FEATURE] new fitur Response

---
+++ src/main/java/com/enigma/tokonyareza/model/response/StoreResponse.java
@@ -0,0 +1,16 @@
+package com.enigma.tokonyareza.model.response;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Builder
+@Getter
+@Setter
+public class StoreResponse {
+
+    private String id;
+    private String storeName;
+    private String address;
+
+}
