18856b73e3c3a7c2cab4a328c30af09710bbb73e
project-java-reza <rizqirezaardiansyahjava@gmail.com>
2023-10-27 12:57:25 +0700

    delete exception duplicateEmail

--- src/main/java/com/enigma/tokonyareza/exception/DuplicateEmailException.java
+++
@@ -1,8 +0,0 @@
-package com.enigma.tokonyareza.exception;
-
-public class DuplicateEmailException extends RuntimeException{
-    public DuplicateEmailException(String message) {
-        super(message);
-    }
-
-}
