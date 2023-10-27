fd84318eb98b228e86f58ad67897d73d70cf32ae
project-java-reza <rizqirezaardiansyahjava@gmail.com>
2023-10-27 14:20:40 +0700

    [FEATURE] new Entity Relasi ProductPrice with Product and Store

---
+++ src/main/java/com/enigma/tokonyareza/entity/ProductPrice.java
@@ -0,0 +1,39 @@
+package com.enigma.tokonyareza.entity;
+
+import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "m_product_price")
+public class ProductPrice {
+
+    @Id
+    @GenericGenerator(strategy = "uuid2", name= "system-uuid")
+    @GeneratedValue(generator = "system-uuid")
+    private String id;
+
+    @Column(nullable = false)
+    private Integer stock;
+
+    @Column(nullable = false)
+    private Long price;
+
+    @Column(name = "is_active", nullable = false)
+    private Boolean isActive;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @ManyToOne
+    @JoinColumn(name = "store_id")
+    private Store store;
+
+}
