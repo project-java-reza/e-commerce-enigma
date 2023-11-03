2c4cc9e40b2515e6d5dfc0d10f460dffcc56d3b7
project-java-reza <rizqirezaardiansyahjava@gmail.com>
2023-11-03 22:17:36 +0700

    [IMPROVE] fix login issue

--- src/main/java/com/enigma/tokonyareza/entity/UserDetailImpl.java
+++ src/main/java/com/enigma/tokonyareza/entity/UserDetailImpl.java
@@ -38,21 +38,21 @@ public class UserDetailImpl implements UserDetails {
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return false;
+        return true;
     }
 }
